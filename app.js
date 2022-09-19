5b866465cfac5f9991e492e524d60fc132f2049f
yoonstar1996 <yagobo1110@naver.com>
2022-09-19 19:01:17 +0900

    09-19-19-00loginHTML

--- app.js
+++ app.js
@@ -2,19 +2,22 @@ const express = require("express");
 const app = express();
 
 app.set("view engine", "ejs");
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // 포트설정 (3000 8080도 가능)
 const port = 8000;
 
-
 // ex - ip:8000/
 app.get("/", (req, res) => {
-    res.render("index");
-})
+  res.render("index");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
 
 // 서버 오픈 명령어
 app.listen(port, () => {
-    console.log("server open:", port);
-})
+  console.log("server open:", port);
+});
