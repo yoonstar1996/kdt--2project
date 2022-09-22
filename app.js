a030e114a8d1504a1d25f895bd660cf6a6822e69
yoonstar1996 <yagobo1110@naver.com>
2022-09-22 22:37:24 +0900

    feat:mypage-item

--- app.js
+++ app.js
@@ -23,7 +23,10 @@ app.get("/", (req, res) => {
 });
 
 app.get("/mypage", (req, res) => {
-  res.render("./login/mypageItem");
+  const data = [
+    
+  ]
+  res.render("./login/mypageItem", data);
 });
 
 app.use(express.static("static"));
