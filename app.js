f9e77cf85dd34805b1ac4d7b4f7ee28c9acfe4dd
DUDURIM <dudurim74@gmail.com>
2022-09-23 16:01:11 +0900

    socket

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const port = 8000;
 
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
-const moment = require('moment');
-require('moment-timezone'); 
+const moment = require("moment");
+require("moment-timezone");
 moment.tz.setDefault("Asia/Seoul");
 
 app.set("view engine", "ejs");
@@ -17,12 +17,29 @@ app.use(express.json());
 const router = require("./routes");
 app.use("/", router);
 
+// 소켓 채팅 시작!
 
+var client_list = {};
+// {소켓아이디 : 닉네임, 소켓아이디 : 닉네임 , .....}
 
-const users = {};
-io.on("connection", (socket) => {});
+io.on("connection", function (socket) {
+  //연결이 들어오면 실행되는 이벤트
+
+  // 입장 메시지 시작!
+
+  socket.on("showchat", (ibjangmsg) => {
+    console.log(ibjangmsg, "님이 입장했습니다.");
+    console.log("Server Socket Connected", `${socket.id}`);
+    io.emit("showchat", `${ibjangmsg.ibjangmsg}님이 입장하셨습니다.`);
+    io.emit("notice", ibjangmsg);
+  });
+
+  // 입장 메시지 끝!
+});
+
+// 소켓 채팅 끝!
 
 // 서버 오픈 명령어
-app.listen(port, () => {
+http.listen(port, () => {
   console.log("server open:", port);
 });
