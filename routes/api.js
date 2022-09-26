0694fe49372c0fe42f7fdd86882a65839439ae21
ymaru <dyun022@gmail.com>
2022-09-26 13:43:50 +0900

    fix: link jq

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,10 @@ const main = require("../controller/Cindex");
 const socket = require("../controller/CSocket");
 const uesr = require("../controller/CUser");
 const product = require("../controller/CProduct");
+
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
 const router = express.Router();
 
 // 로그인 페이지
@@ -10,6 +14,7 @@ router.post("/login", uesr.login_check);
 // 유저 생성
 router.post("/signup", uesr.signup_create);
 router.post("/signup/idcheck", uesr.signup_id_check);
+
 // 상품 생성
 router.post("/product", product.product_create);
 // 소캣 룸 생성
