58fc2e97566870503f4a36da86dbe2bd87c1d789
yoonstar1996 <yagobo1110@naver.com>
2022-09-26 14:02:22 +0900

    fix: DB

--- routes/index.js
+++ routes/index.js
@@ -4,22 +4,6 @@ const socket = require("../controller/CSocket");
 const uesr = require("../controller/CUser");
 const product = require("../controller/CProduct");
 const router = express.Router();
-const multer = require("multer");
-const path = require("path");
-
-const upload = multer({
-  storage: multer.diskStorage({
-    destination(req, file, done) {
-      done(null, "uploads/");
-    },
-    filename(req, file, done) {
-      const ext = path.extname(file.originalname);
-      //   done(null, req.body.id + ext);
-      done(null, file.originalname);
-    },
-  }),
-  limits: { fileSize: 5 * 1024 * 1024 },
-});
 
 router.get("/", main.main);
 
@@ -35,7 +19,7 @@ router.get("/kakao_callback", main.kakaoCallback);
 router.get("/signup", uesr.signup);
 
 // 상품 생성
-router.get("/product", upload.single("userfile"), product.product);
+router.get("/product",  product.product);
 // 소캣
 router.get("/socket", socket.socket);
 
